09bea980fbcbcaa50f7131a0189e8620c6935c58
echasin <echasin@innvosolutions.com>
2015-12-26 21:16:07 -0500

    Code Merge from adap_postgres_mvp_filterdemo7 completed by echasin.   Jira Issues 101-108 need to be addressed by Ali.

--- src/main/webapp/scripts/app/entities/asset/asset-delete-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/asset/asset-delete-dialog.controller.js
@@ -1,17 +1,78 @@
 'use strict';
 
-angular.module('adapApp')
-	.controller('AssetDeleteController', function($scope, $modalInstance, entity, Asset) {
+angular.module('adapApp').controller('AssetDialogController',
+    ['$scope', '$stateParams', '$modalInstance', 'entity', 'Asset', 'Objrecordtype', 'Objclassification', 'Objcategory', 'Objtype', 'Location', 'Score', 'Vulnerability', 'Identifier',
+        function($scope, $stateParams, $modalInstance, entity, Asset, Objrecordtype, Objclassification, Objcategory, Objtype, Location, Score, Vulnerability, Identifier) {
 
         $scope.asset = entity;
+        //$scope.objrecordtypes = Objrecordtype.getrecordtypes();
+        $scope.objclassifications;
+        $scope.objcategorys;
+        $scope.objtypes;
+        $scope.locations = Location.query();
+        $scope.scores = Score.query();
+        $scope.vulnerabilitys = Vulnerability.query();
+        $scope.identifiers = Identifier.query();
+        
+        
+        $scope.getclassifications = function (id) {
+            Objclassification.getclassifications({id: id}, function (result) {
+               $scope.objclassifications = result;
+               $scope.objcategorys="";
+               $scope.objtypes="";
+           });
+        };
+        
+        $scope.getcategories = function (id) {
+        	Objcategory.getcategories({id: id}, function (result) {
+               $scope.objcategorys = result;
+               $scope.objtypes="";
+           });
+        };
+        
+        $scope.gettypes = function (id) {
+        	Objtype.gettypes({id: id}, function (result) {
+               $scope.objtypes = result;
+           });
+        };
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        $scope.load = function(id) {
+            Asset.get({id : id}, function(result) {
+                $scope.asset = result;
+            });
+        };
+
+        var onSaveFinished = function (result) {
+            $scope.$emit('adapApp:assetUpdate', result);
+            $modalInstance.close(result);
+        };
+
+        $scope.save = function () {
+            if ($scope.asset.id != null) {
+                Asset.update($scope.asset, onSaveFinished);
+            } else {
+                Asset.save($scope.asset, onSaveFinished);
+            }
+        };
+
         $scope.clear = function() {
             $modalInstance.dismiss('cancel');
         };
-        $scope.confirmDelete = function (id) {
-            Asset.delete({id: id},
-                function () {
-                    $modalInstance.close(true);
-                });
-        };
-
-    });
+}]);
--- src/main/webapp/scripts/app/entities/asset/asset.js
+++ src/main/webapp/scripts/app/entities/asset/asset.js
@@ -19,7 +19,6 @@ angular.module('adapApp')
                 resolve: {
                     translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
                         $translatePartialLoader.addPart('asset');
-                        $translatePartialLoader.addPart('status');
                         $translatePartialLoader.addPart('global');
                         return $translate.refresh();
                     }]
@@ -41,7 +40,6 @@ angular.module('adapApp')
                 resolve: {
                     translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
                         $translatePartialLoader.addPart('asset');
-                        $translatePartialLoader.addPart('status');
                         return $translate.refresh();
                     }],
                     entity: ['$stateParams', 'Asset', function($stateParams, Asset) {
@@ -92,29 +90,6 @@ angular.module('adapApp')
                         templateUrl: 'scripts/app/entities/asset/asset-dialog.html',
                         controller: 'AssetDialogController',
                         size: 'lg',
-                        resolve: {
-                            entity: ['Asset', function(Asset) {
-                                return Asset.get({id : $stateParams.id});
-                            }]
-                        }
-                    }).result.then(function(result) {
-                        $state.go('asset', null, { reload: true });
-                    }, function() {
-                        $state.go('^');
-                    })
-                }]
-            })
-            .state('asset.delete', {
-                parent: 'asset',
-                url: '/{id}/delete',
-                data: {
-                    authorities: ['ROLE_USER'],
-                },
-                onEnter: ['$stateParams', '$state', '$modal', function($stateParams, $state, $modal) {
-                    $modal.open({
-                        templateUrl: 'scripts/app/entities/asset/asset-delete-dialog.html',
-                        controller: 'AssetDeleteController',
-                        size: 'md',
                         resolve: {
                             entity: ['Asset', function(Asset) {
                                 return Asset.get({id : $stateParams.id});
